f4297614252a076adc2e3da7cf40110e0d4a1d22
Juansezhin <jsstenta@outlook.com>
2025-01-13 11:44:24 -0300

    Refactor lógica del juego PONG

--- sketch.js
+++ sketch.js
@@ -1,197 +1,169 @@
-/** @format */
-
-// sketch.js
-/** @format */
-
-let anchoCanvas = 800;
-let altoCanvas = 400;
-
-let jugadorX = 15;
-let jugadorY;
-let anchoRaqueta = 10;
-let altoRaqueta = 100;
-
-let computadoraX = anchoCanvas - 25;
-let computadoraY;
-
-let pelotaX, pelotaY;
-let diametroPelota = 20;
-let velocidadPelotaX = 5;
-let velocidadPelotaY = 5;
-let anguloPelota = 0;
-
-let grosorMarco = 10;
-
-let jugadorScore = 0;
-let computadoraScore = 0;
-
-let fondo;
-let barraJugador;
-let barraComputadora;
-let bola;
-let sonidoRebote;
+let imagenPelota;
+let imagenRaqueta;
+let imagenComputadora;
+let imagenFondo;
+let sonidoRaqueta;
 let sonidoGol;
 
-function preload() {
-  fondo = loadImage("media/fondo1.png");
-  barraJugador = loadImage("media/barra1.png");
-  barraComputadora = loadImage("media/barra2.png");
-  bola = loadImage("media/bola.png");
-  sonidoRebote = loadSound("media/bounce.wav");
-  sonidoGol = loadSound("media/jingle_win_synth_02.wav");
+let puntosJugador = 0;
+let puntosComputadora = 0;
+
+
+class Pelota {
+    constructor(x, y, diameter, vx, vy) {
+        this.x = x;
+        this.y = y;
+        this.diameter = diameter;
+        //velocidad el sentido inicial de la pelota sea un valor aleatorio, utiliza la función math.random()
+        this.vx = vx;
+        this.vy = vy;
+        this.reset();
+    }
+
+    update() {
+        this.x += this.vx;
+        this.y += this.vy;
+        //aumente la roatación de la pelota con la velocidad en el eje x y la velocidad en el eje y
+        this.rotation += this.vx + this.vy;
+
+        if (this.x > width - this.diameter / 2 || this.x < this.diameter / 2) {
+            sonidoGol.play();
+            if (this.x < width / 2) {
+                puntosComputadora++;
+            } else {
+                puntosJugador++;
+            }
+            narrarPuntos();
+            this.reset();
+        }
+
+        if (this.y > height - this.diameter / 2 || this.y < this.diameter / 2) {
+            this.vy *= -1;
+        }
+
+        //si colisiona con la raqueta del jugador o la computadora, invierte el sentido y aumenta la velocidad en 10%
+        if (colision(this.x, this.y, this.diameter, raqueta.x, raqueta.y, raqueta.width, raqueta.height) || colision(this.x, this.y, this.diameter, computadora.x, computadora.y, computadora.width, computadora.height)) {
+            sonidoRaqueta.play();
+            this.vx *= -1;
+            this.vx *= 1.1;
+            this.vy *= 1.1;
+        }
+
+    }
+
+    reset() {
+        this.x = 400;
+        this.y = 200;
+        this.vx = 5 * (Math.random() < 0.5 ? -1 : 1);
+        this.vy = 5 * (Math.random() < 0.5 ? -1 : 1);
+        //rotación actual de la pelota
+        this.rotation = 0;
+    }
+
+    draw() {
+        //dibuja la pelota como una imagen en lugar de un círculo
+        push();
+        translate(this.x, this.y);
+        rotate(this.rotation);
+        image(imagenPelota, -this.diameter / 2, -this.diameter / 2, this.diameter, this.diameter);
+        pop();
+    }
 }
+
+class Raqueta {
+    constructor(x, y, width, height, speed) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.speed = speed;
+    }
+
+    update() {
+        //raqueta del jugador se mueva con el mouse
+        //identifa si la raqueta es la del jugador, raqueta jugador es la de la izquierda
+        if (this.x < width / 2) {
+            this.y = mouseY;
+        } else {
+            //raqueta de la computadora se mueva siguiendo la pelota
+            if (pelota.y > this.y) {
+                this.y += this.speed;
+            } else {
+                this.y -= this.speed;
+            }
+        }
+        //limitar el movimiento de la raqueta, para que no se salga de la pantalla
+        this.y = constrain(this.y, 0, height - this.height);
+    }
+
+    draw() {
+        //si raqueta jugador dibuja la raqueta con la imagen de la raqueta del jugador
+        if (this.x < width / 2) {
+            image(imagenRaqueta, this.x, this.y, this.width, this.height);
+        } else {
+            //si raqueta computadora dibuja la raqueta con la imagen de la raqueta de la computadora
+            image(imagenComputadora, this.x, this.y, this.width, this.height);
+        }
+    }
+}
+
+let pelota;
+let raqueta;
+let computadora;
+
+//verificar la colisión entre una circunferencia y un rectángulo
+function colision(cx, cy, diameter, rx, ry, rw, rh) {
+    //Si el circulo esta a la izquierda del rectángulo
+    if (cx + diameter / 2 < rx) {
+        return false;
+    }
+    //Si el circulo esta arriba del rectángulo
+    if (cy + diameter / 2 < ry) {
+        return false;
+    }
+    //Si el circulo esta a la derecha del rectángulo
+    if (cx - diameter / 2 > rx + rw) {
+        return false;
+    }
+    //Si el circulo esta abajo del rectángulo
+    if (cy - diameter / 2 > ry + rh) {
+        return false;
+    }
+    return true;
+}
+
+function preload() {
+    imagenPelota = loadImage('media/bola.png');
+    imagenRaqueta = loadImage('media/barra1.png');
+    imagenComputadora = loadImage('media/barra2.png');
+    imagenFondo = loadImage('media/fondo1.png');
+    sonidoRaqueta = loadSound('media/bounce.wav');
+    sonidoGol = loadSound('media/jingle_win_synth_02.wav');
+}
+
 
 function setup() {
-  createCanvas(anchoCanvas, altoCanvas);
-  jugadorY = height / 2 - altoRaqueta / 2;
-  computadoraY = height / 2 - altoRaqueta / 2;
-  resetPelota();
+    createCanvas(800, 400);
+    pelota = new Pelota(400, 200, 50, 5, 5);
+    raqueta = new Raqueta(20, 150, 20, 100, 5);
+    computadora = new Raqueta(760, 150, 20, 100, 5);
 }
+
+function narrarPuntos() {
+    //Narra los puntos utilizando la api speechapi
+    let puntos = "El marcador está " + puntosJugador + " a " + puntosComputadora;
+    let mensaje = new SpeechSynthesisUtterance(puntos);
+    mensaje.lang = 'es-AR';
+    speechSynthesis.speak(mensaje);
+}
+
 
 function draw() {
-  background(fondo);
-  dibujarMarcos();
-  dibujarRaquetas();
-  dibujarPelota();
-  mostrarPuntaje();
-  moverPelota();
-  moverComputadora();
-  verificarColisiones();
-}
-
-function dibujarMarcos() {
-  fill(color("#2B3FD6"));
-  rect(0, 0, width, grosorMarco); // Marco superior
-  rect(0, height - grosorMarco, width, grosorMarco); // Marco inferior
-}
-
-function dibujarRaquetas() {
-  image(barraJugador, jugadorX, jugadorY, anchoRaqueta, altoRaqueta);
-  image(
-    barraComputadora,
-    computadoraX,
-    computadoraY,
-    anchoRaqueta,
-    altoRaqueta
-  );
-}
-
-function dibujarPelota() {
-  push();
-  translate(pelotaX, pelotaY);
-  rotate(anguloPelota);
-  imageMode(CENTER);
-  image(bola, 0, 0, diametroPelota, diametroPelota);
-  pop();
-}
-
-function mostrarPuntaje() {
-  textSize(32);
-  textAlign(CENTER, CENTER);
-  fill(color("#2B3FD6"));
-  text(jugadorScore, width / 4, grosorMarco * 3);
-  text(computadoraScore, (3 * width) / 4, grosorMarco * 3);
-}
-
-function moverPelota() {
-  pelotaX += velocidadPelotaX;
-  pelotaY += velocidadPelotaY;
-
-  // Ajustar el ángulo de la pelota en función de su velocidad
-  let velocidadTotal = sqrt(
-    velocidadPelotaX * velocidadPelotaX + velocidadPelotaY * velocidadPelotaY
-  );
-  anguloPelota += velocidadTotal * 0.05;
-
-  // Colisión con el marco superior e inferior
-  if (
-    pelotaY - diametroPelota / 2 < grosorMarco ||
-    pelotaY + diametroPelota / 2 > height - grosorMarco
-  ) {
-    velocidadPelotaY *= -1;
-  }
-}
-
-function moverComputadora() {
-  if (velocidadPelotaX > 0) {
-    if (pelotaY > computadoraY + altoRaqueta / 2) {
-      computadoraY += 4;
-    } else if (pelotaY < computadoraY + altoRaqueta / 2) {
-      computadoraY -= 4;
-    }
-    computadoraY = constrain(
-      computadoraY,
-      grosorMarco,
-      height - grosorMarco - altoRaqueta
-    );
-  }
-}
-
-function verificarColisiones() {
-  // Colisión con la raqueta del jugador
-  if (
-    pelotaX - diametroPelota / 2 < jugadorX + anchoRaqueta &&
-    pelotaY > jugadorY &&
-    pelotaY < jugadorY + altoRaqueta
-  ) {
-    let puntoImpacto = pelotaY - (jugadorY + altoRaqueta / 2);
-    let factorAngulo = ((puntoImpacto / (altoRaqueta / 2)) * PI) / 3; // Ángulo máximo de 60 grados
-    velocidadPelotaY = 10 * sin(factorAngulo);
-    velocidadPelotaX *= -1;
-    sonidoRebote.play(); // Reproducir sonido de rebote
-  }
-
-  // Colisión con la raqueta de la computadora
-  if (
-    pelotaX + diametroPelota / 2 > computadoraX &&
-    pelotaY > computadoraY &&
-    pelotaY < computadoraY + altoRaqueta
-  ) {
-    let puntoImpacto = pelotaY - (computadoraY + altoRaqueta / 2);
-    let factorAngulo = ((puntoImpacto / (altoRaqueta / 2)) * PI) / 3; // Ángulo máximo de 60 grados
-    velocidadPelotaY = 10 * sin(factorAngulo);
-    velocidadPelotaX *= -1;
-    sonidoRebote.play(); // Reproducir sonido de rebote
-  }
-
-  // Colisión con los bordes izquierdo y derecho (anotación y reinicio)
-  if (pelotaX < 0) {
-    computadoraScore++;
-    sonidoGol.play(); // Reproducir sonido de gol
-    narrarMarcador(); // Narrar marcador
-    resetPelota();
-  } else if (pelotaX > width) {
-    jugadorScore++;
-    sonidoGol.play(); // Reproducir sonido de gol
-    narrarMarcador(); // Narrar marcador
-    resetPelota();
-  }
-}
-
-function narrarMarcador() {
-  let narrador = new SpeechSynthesisUtterance(
-    `El marcador es ${jugadorScore} a ${computadoraScore}`
-  );
-  window.speechSynthesis.speak(narrador);
-}
-
-function resetPelota() {
-  pelotaX = width / 2;
-  pelotaY = height / 2;
-  velocidadPelotaX = 5 * (Math.random() > 0.5 ? 1 : -1);
-  velocidadPelotaY = 5 * (Math.random() > 0.5 ? 1 : -1);
-  anguloPelota = 0;
-}
-
-function keyPressed() {
-  if (keyCode === UP_ARROW || keyCode === 87) {
-    jugadorY -= 50;
-  } else if (keyCode === DOWN_ARROW || keyCode === 83) {
-    jugadorY += 50;
-  }
-  jugadorY = constrain(
-    jugadorY,
-    grosorMarco,
-    height - grosorMarco - altoRaqueta
-  );
+    image(imagenFondo, 0, 0, width, height);
+    pelota.update();
+    pelota.draw();
+    raqueta.update();
+    raqueta.draw();
+    computadora.update();
+    computadora.draw();
 }
